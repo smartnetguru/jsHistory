065c93fa961d8798364d6e433b97635f94a8f02a
Olivier Beg <olivier.beg@emesa.nl>
2014-12-19 12:16:37 +0100

    Updated lusu.js - back button.
    * Back button only worked once because I used a history replace where it
    shouldn't and because of that it kept the earlier page as the only page
    that was opened.
    * lusu.min.js is the minified version.

--- lusu.js
+++ lusu.js
@@ -1,76 +1,156 @@
+/*
+ * This file is licensed and opensourced by Olivier Beg on github.
+ * @source: https://github.com/smiegles/Javascript-history
+ * @copyright: 2014
+ */
 var lusu = {
-  
-  'get' : function(page, callback) {
-    request = new XMLHttpRequest();
-    request.open('GET', page, true);
-
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400){
-        if(typeof callback == 'function') {
-          callback(request.responseText);
-        } else {
-          return request.responseText;
-        }
-      } else {
-        return false;
-      }
-    };
-
-    request.onerror = function() {
-      return false;
-    };
-
-    request.send();
-  },
-  'require': function(script) {
-  
-  },
-  'doc': {
-    'ready' : function(callback) {
-      document.addEventListener('DOMContentLoaded', function() {
-        if(typeof callback == 'function') {
-          return callback();
-        }
-        
-        return true;
-      });
-    }
-  },
-  'history' : {
-    'regex' : /<body\s*[^>]*>([\S\s]*?)<\/body>/i,
-    'push': function(page, callback) {
-      if(page != undefined) {
-        if (history && history.pushState) {
-          history.pushState(page, '', page);
-          
-          this.historyDone(page);
-          
-          if(typeof callback == 'function') {
-            callback();
-          } else {
-            return true;
-          }
-        }
-      } 
+    /*
+     * @function initialize
+     * @desc constructor function, will be called when a history push is requested or the page is called for the first time,
+     * @@returns {void} 
+     */
+    'initialize': function () {
+        lusu.add.eventListeners('a');
     },
-    'historyDone': function(page) {
-      var regex = this.regex;
+    /*
+     * @desc get request
+     * @param string page
+     * @param function callback
+     * @returns mixed
+     */
+    'get': function (page, callback) {
+        request = new XMLHttpRequest();
+        request.open('GET', page, true);
 
-      lusu.get(page, function(response) {
-        var body = regex.exec(response);
-        var body = (body != null ? body[1] : response);
-        
-        document.body.innerHTML = body;
-      });
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+                if (typeof callback === 'function') {
+                    callback(request.responseText);
+                }
+                else if (typeof callback === 'object') {
+                    if (callback['success'] !== undefined && typeof callback['success'] === 'function') {
+                        callback['success'](request.responseText);
+                    }
+                } else {
+                    return request.responseText;
+                }
+            } else {
+                if (callback['error'] !== undefined && typeof callback['error'] === 'function') {
+                    callback['error']({
+                        'error': 'request status was invalid.'
+                    });
+                }
+            }
+        };
+
+        request.onerror = function (e) {
+            if (callback['error'] !== undefined && typeof callback['error'] === 'function') {
+                callback['error'](e);
+            }
+        };
+
+        request.send();
     },
-    'change': function() {
-      window.addEventListener('popstate', function(e) {
-        if(e.state != null) {
-          lusu.history.push(e.state);
-        } else {
-          lusu.history.push('');
+    'doc': {
+        /*
+         * @desc Calls when document is ready
+         * @param function callback
+         * @returns void
+         */
+        'ready': function (callback) {
+            if (document.addEventListener) {
+                document.addEventListener('DOMContentLoaded', callback);
+            } else {
+                document.attachEvent('onreadystatechange', function () {
+                    if (document.readyState === 'interactive') {
+                        callback();
+                    }
+                });
+            }
         }
-      });
+    },
+    'history': {
+        /*
+         * @var regex to explode the body.
+         */
+        'regex': /<body\s*[^>]*>([\S\s]*?)<\/body>/i,
+        /*
+         * @desc changes the history url if history.pushState exists.
+         * @param string page
+         * @returns void
+         */
+        'push': function (page) {
+            if (page !== undefined) {
+                if (history && history.pushState) {
+                    history.pushState({ 'page': page }, '', page);
+                    this.historyDone(page);
+                }
+            }
+        },
+        /*
+         * @desc set's the inner body to the returned value.
+         * @param string page
+         */
+        'historyDone': function (page) {
+            lusu.get(page, function (response) {
+                document.body.innerHTML = lusu.history.getBody(response);
+                lusu.initialize();
+            });
+        },
+        /*
+         * returns the inner content of the body element if exists, otherwise returns the entire content.
+         * @param string response
+         * @returns {lusu.history@pro;regex@call;exec|lusu.history.getBody.body|@arr;body}
+         */
+        'getBody': function (response) {
+            var body = this.regex.exec(response);
+            var body = (body !== null ? body[1] : response);
+
+            return body;
+        },
+        /*
+         * @desc When the document changes this function will be called to change to url again.
+         */
+        'change': function () {
+            window.addEventListener('popstate', function (e) {
+              lusu.history.historyDone(e.state.page);
+            });
+        }
+    },
+    'add': {
+        /*
+         * @desc Contains all the elements from the document.
+         */
+        'elements': {} || [],
+        /*
+         * @desc binds the push function to a certain selector
+         * @param string selector
+         * @returns void
+         */
+        'eventListeners': function (selector) {
+            this.elements = document.querySelectorAll(selector);
+            Array.prototype.forEach.call(this.elements, function (element) {
+                element.addEventListener('click', function (event) {
+                    if (element.getAttribute('href') !== undefined) {
+                        event.preventDefault();
+                        lusu.history.push(element.getAttribute('href'));
+                    }
+                }, false);
+            });
+        },
+        /*
+         * @desc unserts the old selectors given by the elements variable.
+         * @returns void
+         */
+        'unset': function () {
+            for (var i = 0; i < this.elements.length; i++) {
+                this.elements[i].removeEventListener('click');
+            }
+        }
     }
-  }
 };
+lusu.history.change();
+
+lusu.doc.ready(function () {
+    lusu.initialize();
+});
